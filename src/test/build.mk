cdfaec0a9d20c2ac2bdbe176dd0b38172871a422
Hadi Moshayedi <hadi@ubicloud.com>
2024-02-07 17:24:57 -0800

    Restructure the project
    1. Changes the project structure and puts all source files under
    directories in src.
    2. Makes the Makefile a bit nicer and easier to navigate.
    3. Refactors test_ubi into 3 different files to make it more manageable.

---
+++ src/test/build.mk
@@ -0,0 +1,40 @@
+
+TEST_DIR := $(SRC_DIR)/test
+TEST_BIN_DIR = $(BIN_DIR)/test
+DATA_TARGETS = $(TEST_BIN_DIR)/test_image.raw $(TEST_BIN_DIR)/test_disk.raw
+TEST_TARGETS = $(TEST_BIN_DIR)/test_ubi $(TEST_BIN_DIR)/memcheck_ubi $(DATA_TARGETS)
+
+TEST_BDEVS := --bdev ubi0 --bdev ubi_nosync --bdev ubi_directio --bdev ubi_copy_on_read
+
+$(TEST_BIN_DIR)/test_image.raw:
+	$(info Building $@ ...)
+	@mkdir -p $(@D)
+	@dd if=/dev/random of=$@ bs=1048576 count=40
+
+$(TEST_BIN_DIR)/test_disk.raw: $(TEST_BIN_DIR)/test_image.raw
+	$(info Building $@ ...)
+	@mkdir -p $(@D)
+	@cp $< $@
+	@truncate --size 100M $@
+
+$(TEST_BIN_DIR)/memcheck_ubi: $(TEST_DIR)/memcheck_ubi/*.c $(LIB_OBJS)
+	$(info Building $@ ...)
+	@mkdir -p $(@D)
+	@$(CC) $(CFLAGS) $? -o $@ $(LDFLAGS)
+
+$(TEST_BIN_DIR)/test_ubi: $(TEST_DIR)/test_ubi/*.c $(LIB_OBJS)
+	$(info Building $@ ...)
+	@mkdir -p $(@D)
+	@$(CC) $(CFLAGS) -I$(TEST_DIR)/test_ubi $? -o $@ $(LDFLAGS)
+
+check: $(TEST_BIN_DIR)/test_ubi $(DATA_TARGETS)
+	sudo $(TEST_BIN_DIR)/test_ubi --cpumask [0,1,2] --json $(TEST_DIR)/test_conf.json \
+		--json-ignore-init-errors $(TEST_BDEVS)
+
+valgrind: $(TEST_BIN_DIR)/memcheck_ubi $(DATA_TARGETS)
+	sudo valgrind $(TEST_BIN_DIR)/memcheck_ubi --cpumask [0] \
+		--json-ignore-init-errors --json $(TEST_DIR)/test_conf.json $(TEST_BDEVS)
+
+coverage:
+	lcov --capture --directory . --exclude=`pwd`/$(TEST_DIR)/'*.c' --no-external --output-file coverage.info > /dev/null
+	genhtml coverage.info --output-directory coverage_report
---
+++ src/test/test_ubi/init_thread.c
@@ -0,0 +1,97 @@
+#include "test_ubi.h"
+
+#define DEFAULT_BDEV_NAME "ubi0"
+
+enum dd_cmdline_opts {
+    MEMCHECK_OPTION_BDEV = 0x1000,
+};
+
+static struct option g_cmdline_opts[] = {{
+                                             .name = "bdev",
+                                             .has_arg = 1,
+                                             .flag = NULL,
+                                             .val = MEMCHECK_OPTION_BDEV,
+                                         },
+                                         {.name = NULL}};
+
+static struct test_opts g_opts;
+
+static void test_ubi_run(void *arg1) {
+    int i;
+
+    struct spdk_cpuset tmpmask = {};
+
+    if (spdk_env_get_core_count() < 3) {
+        spdk_app_stop(-1);
+        return;
+    }
+
+    g_opts.init_thread = NULL;
+    g_opts.io_thread = NULL;
+    g_opts.ut_thread = NULL;
+
+    SPDK_ENV_FOREACH_CORE(i) {
+        if (i == spdk_env_get_current_core()) {
+            g_opts.init_thread = spdk_get_thread();
+            continue;
+        }
+        spdk_cpuset_zero(&tmpmask);
+        spdk_cpuset_set_cpu(&tmpmask, i, true);
+        if (g_opts.ut_thread == NULL) {
+            g_opts.ut_thread = spdk_thread_create("ut_thread", &tmpmask);
+        } else if (g_opts.io_thread == NULL) {
+            g_opts.io_thread = spdk_thread_create("io_thread", &tmpmask);
+        }
+    }
+
+    spdk_thread_send_msg(g_opts.ut_thread, run_ut_thread, &g_opts);
+}
+
+void stop_init_thread(void *arg) { spdk_app_stop(arg == NULL ? 0 : -1); }
+
+static void usage(void) { printf("  -bdev Block device to be used for testing.\n"); }
+
+static int parse_arg(int argc, char *argv) {
+    switch (argc) {
+    case MEMCHECK_OPTION_BDEV:
+        if (g_opts.n_bdevs >= MAX_BDEVS) {
+            fprintf(stderr, "Too many bdevs.\n");
+            exit(-1);
+        }
+        g_opts.bdev_names[g_opts.n_bdevs++] = strdup(argv);
+        break;
+    default:
+        return -EINVAL;
+    }
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int rc;
+    struct spdk_app_opts opts = {};
+    spdk_app_opts_init(&opts, sizeof(opts));
+    opts.name = "test_ubi";
+    opts.reactor_mask = "0x1";
+
+    rc = spdk_app_parse_args(argc, argv, &opts, NULL, g_cmdline_opts, parse_arg, usage);
+    if (rc != SPDK_APP_PARSE_ARGS_SUCCESS) {
+        exit(rc);
+    }
+
+    if (g_opts.n_bdevs == 0) {
+        g_opts.n_bdevs = 1;
+        g_opts.bdev_names[0] = strdup(DEFAULT_BDEV_NAME);
+    }
+
+    rc = spdk_app_start(&opts, test_ubi_run, NULL);
+    if (rc) {
+        SPDK_ERRLOG("Error occured while testing bdev_ubi.\n");
+    }
+
+    for (int i = 0; i < g_opts.n_bdevs; i++)
+        free(g_opts.bdev_names[i]);
+
+    spdk_app_fini();
+
+    return rc;
+}
